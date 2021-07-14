0f456727c83bb7e193333a34d4dbaec0a05fe014
hustllz999 <526918351@qq.com>
2021-07-14 22:22:45 +0800

    0714test_one

--- src/test.scala
+++ src/test.scala
@@ -1,25 +1,15 @@
-import java.sql.Date
 
-import test.date
 
 object test {
    val a:Int=20
 
-  var date() = 33
+  var date = 33
 
   def main(args: Array[String]): Unit = {
     println("hello,world!")
-    private val hello
 
-    Date date = new Date()
-    val note=new Array[Int => Int](4)
-    while(i)
-  }
-
-  object date{
 
   }
-
 }
 
 class note{
