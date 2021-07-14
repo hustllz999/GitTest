e1c2067aa18625180787d3a7f1a6c8e8b44e5ce9
hustllz999 <526918351@qq.com>
2021-07-14 22:17:06 +0800

    0714test_one

---
+++ src/test.scala
@@ -0,0 +1,27 @@
+import java.sql.Date
+
+import test.date
+
+object test {
+   val a:Int=20
+
+  var date() = 33
+
+  def main(args: Array[String]): Unit = {
+    println("hello,world!")
+    private val hello
+
+    Date date = new Date()
+    val note=new Array[Int => Int](4)
+    while(i)
+  }
+
+  object date{
+
+  }
+
+}
+
+class note{
+  println("hello,guood")
+}
